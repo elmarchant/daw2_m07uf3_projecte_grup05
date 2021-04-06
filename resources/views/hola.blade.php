04b7897efb0c80b8bc9b8e896164be90a3910c55
Webmaster <webmaster@webserver.fjeclot.net>
2021-04-06 20:46:37 +0200

    Other commit

---
+++ resources/views/hola.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola holita</title>
+</head>
+<body>
+    <icon></icon>
+    <h1><span style="color:red">Elver Galarga</span> estuvo aquí</h1>
+</body>
+</html>
